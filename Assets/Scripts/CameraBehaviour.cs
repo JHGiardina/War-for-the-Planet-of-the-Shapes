9f7a3fea00d328804f5eef03acdeb78a89ef3d53
AustinTackett <117427861+AustinTackett@users.noreply.github.com>
2025-03-31 15:52:16 -0500

    Update CameraBehavior

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -2,32 +2,33 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
 {
-    public GameObject followObject;
+    private Vector3 target;
 
     void Start()
     {
-        
+        // Don't move the camera anywhere on start
+        target = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, 100);
     }
 
     void LateUpdate()
     {
-        // Ignore y coordinates
-        Vector3 target = GetMouseOn3DWorld();
-        target = new Vector3(target.x, transform.position.y, target.z);
-
-        transform.position = Vector3.MoveTowards(transform.position, target, 10);
+        // Yes I could use input actions but this works for now
+        if(Input.GetMouseButtonDown(0))
+        {
+            target = GetMouseTargetPosition();
+        }
     }
 
-    private Vector3 GetMouseOn3DWorld()
+    private Vector3 GetMouseTargetPosition()
     {
         //https://stackoverflow.com/questions/72975015/how-do-i-get-mouse-world-position-x-y-plane-only-in-unity
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
-        Debug.Log(worldPosition);
-        return worldPosition;
-    }
-
-    private void MoveTo(Vector3 position)
-    {
-        
+        Vector3 target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        target = new Vector3(target.x, transform.position.y, target.z);
+        return target;
     }
 }
