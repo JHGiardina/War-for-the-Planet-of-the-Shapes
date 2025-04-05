e5ca8264d8e652e4e76261729be7a3b6d4023f31
AustinTackett <117427861+AustinTackett@users.noreply.github.com>
2025-04-05 13:01:20 -0500

    Added Weighted Spawning

--- Assets/Scripts/EnemySpawning.cs
+++ Assets/Scripts/EnemySpawning.cs
@@ -9,6 +9,11 @@ public class WaveManager : MonoBehaviour
     public float TimeBetweenWaves = 5f;
     public int EnemiesPerWave = 5;
 
+    // Probability a tanked is spawned is tankSpawnFactor / (tankSpawnFactor + rangedSpawnFact + meleeSpawnFactor)
+    public int tankSpawnFactor = 1;
+    public int rangedSpawnFactor = 5;
+    public int meleeSpawnFactor = 5;
+
     private float countdown = 0f;
     private int waveNumber = 0;
 
@@ -37,7 +42,11 @@ public class WaveManager : MonoBehaviour
     {
         // Pick a random spawn point and enemy type
         int spawnIndex = Random.Range(0, SpawnPoints.Length);
-        int enemyIndex = Random.Range(0, Enemy.Length);
+
+        // Yes if you need add an additional unit you need to add an additional weight
+        int[] EnemyWeights = {meleeSpawnFactor, rangedSpawnFactor, tankSpawnFactor};
+        int enemyIndex = GetWeightedRandomIndex(EnemyWeights);
+        Debug.Log(enemyIndex);
 
         // Spawn the enemy
         GameObject newEnemy = Instantiate(Enemy[enemyIndex],
@@ -55,4 +64,33 @@ public class WaveManager : MonoBehaviour
             barScript.targetHealth = unitHealth;
         }
     }
+
+    // This will throw an out of bounds error if weights is a zero length array
+    private int GetWeightedRandomIndex(int[] weights)
+    {
+        //https://leetcode.com/problems/random-pick-with-weight/description/
+
+        // Get Prefix Sum of Weights
+        int[] prefixSum = new int[weights.Length];
+        prefixSum[0] = weights[0];
+        for(int i = 1; i < prefixSum.Length; i++)
+        {
+            prefixSum[i] = weights[i] + prefixSum[i-1];
+        }
+
+        // Select random value using weights
+        
+        int totalWeights = prefixSum[prefixSum.Length - 1];
+        Debug.Log(totalWeights);
+        int randomNum = Random.Range(0, totalWeights);
+        for(int i = 0; i < prefixSum.Length; i++)
+        {
+            if(randomNum < prefixSum[i])
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }   
 }
