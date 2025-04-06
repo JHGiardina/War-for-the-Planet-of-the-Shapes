2766510e8e359e1fa083e9ea55dba1e228e0733a
JHGiardina <JHGiardina@gmail.com>
2025-04-06 18:55:05 -0500

    Base Targeting

--- Assets/Scripts/Human Variations/BaseHumanUnitBehaviour.cs
+++ Assets/Scripts/Human Variations/BaseHumanUnitBehaviour.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Linq;
 
 // Basic implementations so the user only needs to implement the hit method
 
@@ -16,6 +17,10 @@ public abstract class BaseHumanUnitBehaviour : MonoBehaviour
 
     private UnitHealthBar healthBar;
     private string PRISM_TAG = "Prism";
+    private string BASE_TAG = "PrismBase";
+
+    public float detectionRadius = 10f;
+    private GameObject targetBase;
 
     // Just implement this to create your unit
     public abstract void Hit();
@@ -28,6 +33,14 @@ public abstract class BaseHumanUnitBehaviour : MonoBehaviour
         Collider = GetComponent<Collider>();
         healthBar = GetComponentInChildren<UnitHealthBar>();
         
+        var baseObj = GameObject.FindWithTag(BASE_TAG);
+        if (baseObj != null)
+        {
+            targetBase = baseObj;
+        }else
+        {
+            Debug.LogError($"[{name}] No GameObject with tag '{BASE_TAG}' found in scene!");
+        }
     }
 
     public virtual void Start()
@@ -43,21 +56,39 @@ public abstract class BaseHumanUnitBehaviour : MonoBehaviour
         // Try a hit within our range (will go through walls but the range is so small this is intended)
         Hit();
 
-         // Get a human target if we don't have one or its dead else move towards human target
-        if(prismTarget == null)
-        {
-            FindPrismTarget();
-        }
-        else
-        {
-            // Jittering and glitchy should use raycasts instead 
-            // Don't let the prism move closer than a specific radius
-            //Vector3 humanToPrismDirection = (prismTarget.transform.position  - transform.position).normalized;
-            //Vector3 targetPosition = prismTarget.transform.position - (MinApproachRadius * humanToPrismDirection);
-            //navMeshAgent.SetDestination(targetPosition);
+        //Check radius for target
+        Collider[] hits= Physics.OverlapSphere(transform.position, detectionRadius);
+        var prismsInRange = hits
+            .Where(h => h.CompareTag(PRISM_TAG))
+            .Select(h => h.transform.gameObject);
 
-            navMeshAgent.SetDestination(prismTarget.transform.position);
+        if (prismsInRange.Any())
+        {
+            prismTarget = prismsInRange
+                .OrderBy(prismsInRange => Vector3.Distance(transform.position, prismsInRange.transform.position))
+                .First();
+        }else
+        {
+            prismTarget = targetBase;
         }
+
+        navMeshAgent.SetDestination(prismTarget.transform.position);
+
+         // Get a human target if we don't have one or its dead else move towards human target
+        // if(prismTarget == null)
+        // {
+        //     FindPrismTarget();
+        // }
+        // else
+        // {
+        //     // Jittering and glitchy should use raycasts instead 
+        //     // Don't let the prism move closer than a specific radius
+        //     //Vector3 humanToPrismDirection = (prismTarget.transform.position  - transform.position).normalized;
+        //     //Vector3 targetPosition = prismTarget.transform.position - (MinApproachRadius * humanToPrismDirection);
+        //     //navMeshAgent.SetDestination(targetPosition);
+
+        //     navMeshAgent.SetDestination(prismTarget.transform.position);
+        // }
 
         //Debug.Log(navMeshAgent.velocity.magnitude);
         if(navMeshAgent.velocity.magnitude >= 0 && animator != null)
