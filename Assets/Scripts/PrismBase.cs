5ab06e686a80bdc1d9c125a2dfa37fb29a50dd53
JHGiardina <JHGiardina@gmail.com>
2025-04-07 12:38:56 -0500

    Maybe death?
    Im too impatient to wait to test it out

---
+++ Assets/Scripts/PrismBase.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrismBase : MonoBehaviour
+{
+    public PrismUnitBehaviour prismBehavior;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        prismBehavior = GetComponentInChildren<PrismUnitBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (prismBehavior.Health <= 0)
+        {
+            SceneManager.LoadScene("DefeatScene");
+        }
+    }
+}
