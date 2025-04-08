60ac34dccf1b6cd549a4ea90fdbccf9d6b8c0665
EN0M1 <eli.mizerski@gmail.com>
2025-04-07 19:45:23 -0500

    Added difficulty script to save diff

---
+++ Assets/Scripts/DifficultyManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using TMPro;
+
+public enum Difficulty
+{
+    Easy,
+    Medium,
+    Hard
+}
+
+public class DifficultyManager : MonoBehaviour
+{
+
+    private static DifficultyManager _instance;
+
+    public static DifficultyManager GetInstance()
+    {
+        return _instance;
+    }
+
+    public Difficulty DifficultyLevel = Difficulty.Easy;
+    public TextMeshProUGUI difficultyText;
+
+    void Awake()
+    {
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        UpdateDifficultyText();
+        ApplyDifficulty(DifficultyLevel);
+    }
+
+    public void NewDifficultySelected(Difficulty diflvl)
+    {
+        DifficultyLevel = diflvl;
+        ApplyDifficulty(diflvl);
+        UpdateDifficultyText();
+        Debug.Log("Difficulty selected: " + diflvl);
+    }
+
+    private void ApplyDifficulty(Difficulty level)
+    {
+        // game behavior here
+        Debug.Log("Applying difficulty: " + level);
+    }
+
+    private void UpdateDifficultyText()
+    {
+        if (difficultyText != null)
+        {
+            difficultyText.text = "Current Difficulty: " + DifficultyLevel.ToString();
+        }
+    }
+}
